35d5705861727d8cd760a2b413a6ed314b5bd523
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-09-11 13:20:14 +0100

    Remove stderr from tests
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/btrfs-progs_works.rb
+++ controls/btrfs-progs_works.rb
@@ -19,7 +19,6 @@ control 'core-plans-btrfs-progs-works' do
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stderr') { should be_empty }
   end
   
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
