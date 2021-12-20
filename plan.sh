32f75f896d6e358b3117c213bd04dc9e5f26d54e
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-12-20 21:03:23 +0530

    btrfs-progs bumped from 5.11 to 5.15.1
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- plan.sh
+++ plan.sh
@@ -1,10 +1,10 @@
 pkg_name="btrfs-progs"
 pkg_origin="core"
-pkg_version="5.11"
+pkg_version="5.15"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPLv2")
 pkg_source="https://git.kernel.org/pub/scm/linux/kernel/git/kdave/btrfs-progs.git/snapshot/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="18b8569efd9b234c6b4ccbda9797a44a884f54c92fb81c7868bceec82ed3a6f5"
+pkg_shasum="ffa2df3ce6de19cbc2ab58a27018662e3558f16c9cb43eafab3203df2b0f008d"
 pkg_deps=(core/glibc
           core/util-linux
           # NOTE(ssd) 2020-05-11: This dependency must be listed
@@ -27,7 +27,8 @@ pkg_build_deps=(core/make
                 core/autoconf
                 core/automake
                 core/pkg-config
-                core/python)
+                core/python
+                core/systemd)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
@@ -38,6 +39,6 @@ do_build() {
     AL_OPTS="-I $(pkg_path_for core/pkg-config)/share/aclocal -I$(pkg_path_for core/automake)/share/aclocal-1.16"
     export AL_OPTS
     ./autogen.sh
-    ./configure --disable-documentation --prefix="${pkg_prefix}"
+    ./configure --disable-documentation --disable-zoned --prefix="${pkg_prefix}"
     make
 }
